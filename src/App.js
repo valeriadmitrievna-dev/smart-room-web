1e587542e0902cc59284312aeeb28dcaddd97de0
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-12-22 02:42:27 +0300

    Menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import Menu from "@/components/Menu";
 
 function App() {
   return (
     <>
       <Menu />
-      <div>content</div>
+      <Routes>
+        <Route exact path="/dashboard" element={<div>dashboard</div>} />
+        <Route exact path="/members" element={<div>members</div>} />
+        <Route exact path="/chat" element={<div>chat</div>} />
+        <Route exact path="/devices" element={<div>devices</div>} />
+        <Route exact path="/settings" element={<div>settings</div>} />
+        <Route exact path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
       <div>side</div>
     </>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import s from "@styles/menu.module.scss";
+import { Link, useLocation } from "react-router-dom";
+
+const routes = ["dashboard", "members", "chat", "devices", "settings"];
 
 export default function Menu() {
-    return (
-        <nav>
-            menu
-        </nav>
-    )
+  const pathname = useLocation().pathname.replace('/', '');
+  
+  return (
+    <nav className={s.navigation}>
+      {routes.map(route => (
+        <Link to={`/${route}`}>
+          <span data-active={pathname.startsWith(route)}>{route}</span>
+        </Link>
+      ))}
+    </nav>
+  );
 }
