97d75073e48806b5b7161603b5dafc42200603ad
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2021-12-22 05:21:53 +0300

    Update menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,16 +2,40 @@ import React from "react";
 import s from "@styles/menu.module.scss";
 import { Link, useLocation } from "react-router-dom";
 
-const routes = ["dashboard", "members", "chat", "devices", "settings"];
+const routes = [
+  {
+    path: "dashboard",
+    icon: "apps-outline",
+  },
+  {
+    path: "members",
+    icon: "people-outline",
+  },
+  {
+    path: "chat",
+    icon: "chatbubble-outline",
+  },
+  {
+    path: "devices",
+    icon: "cube-outline",
+  },
+  {
+    path: "settings",
+    icon: "settings-outline",
+  },
+];
 
 export default function Menu() {
-  const pathname = useLocation().pathname.replace('/', '');
-  
+  const pathname = useLocation().pathname.replace("/", "");
+
   return (
     <nav className={s.navigation}>
       {routes.map(route => (
-        <Link to={`/${route}`}>
-          <span data-active={pathname.startsWith(route)}>{route}</span>
+        <Link to={`/${route.path}`}>
+          <p data-active={pathname.startsWith(route.path)}>
+            <ion-icon name={route.icon}></ion-icon>
+            <span>{route.path}</span>
+          </p>
         </Link>
       ))}
     </nav>
